d07e9bd2bd646ba5f0dc55bfc55ee2e58a2ad6d8
Nedal <nedalosama3@gmail.com>
2024-07-18 22:11:06 +0300

    StringBuilder

--- Assignment 4/Program.cs
+++ Assignment 4/Program.cs
@@ -2,6 +2,7 @@
 using Assignment_4.interface_Ex2;
 using Assignment_4.Interface_Ex3;
 using System;
+using System.Text;
 
 namespace Assignment_4
 {
@@ -27,23 +28,80 @@ namespace Assignment_4
             #endregion
 
             #region Ex#_interface
-            Airplane airplane = new Airplane();
+            //Airplane airplane = new Airplane();
             //   airplane.forword();
-            IMovable movable = new Airplane();
-            movable.forword();
+            //IMovable movable = new Airplane();
+            //movable.forword();
             #endregion
+
+
             #region deep copy Vs Shallo copy 
-            int[] arr1 = [1, 3, 5];
-            int[] arr2 = { 4, 6, 7 };
+            // int[] arr1 = [1, 3, 5];
+            //int[] arr2 = { 4, 6, 7 };
             //Shallo copy 
             //Console.WriteLine(arr1.GetHashCode());
             //Console.WriteLine(arr2.GetHashCode());
             //arr1 = arr2;
-
             // deep copy
-            arr2 = (int[])arr1.Clone();
+            // arr2 = (int[])arr1.Clone();
 
             #endregion
+            #region  StringBuilder
+            // Shallow copy
+            /*   StringBuilder[] array1 = new StringBuilder[]
+           {
+               new StringBuilder("Hello"),
+               new StringBuilder("World")
+           };
+
+
+               StringBuilder[] array2 = (StringBuilder[])array1.Clone();
+
+               array2[0].Append(" Everyone");
+
+               Console.WriteLine("Array 1:");
+               foreach (var sb in array1)
+               {
+                   Console.WriteLine(sb.ToString());
+               }
+
+               Console.WriteLine("Array 2:");
+               foreach (var sb in array2)
+               {
+                   Console.WriteLine(sb.ToString());
+               }*/
+            /////////////////////////////////////////////////
+            // Deep copy
+         /*   StringBuilder[] array1 = new StringBuilder[]
+            {
+            new StringBuilder("Hello"),
+            new StringBuilder("World")
+            };
+
+          
+            StringBuilder[] array2 = new StringBuilder[array1.Length];
+            for (int i = 0; i < array1.Length; i++)
+            {
+                array2[i] = new StringBuilder(array1[i].ToString());
+            }
+
+           
+            array2[0].Append(" Everyone");
+
+            Console.WriteLine("Array 1:");
+            foreach (var sb in array1)
+            {
+                Console.WriteLine(sb.ToString());
+            }
+
+            Console.WriteLine("Array 2:");
+            foreach (var sb in array2)
+            {
+                Console.WriteLine(sb.ToString());
+            }*/
+            //////////////////////////////
+            #endregion
+
         }
     }
 }
