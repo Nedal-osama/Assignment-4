1604ea08375d6cc31987cc18106ce882495493b1
Nedal <nedalosama3@gmail.com>
2024-07-18 21:25:08 +0300

    part1_interface

--- Assignment 4/Program.cs
+++ Assignment 4/Program.cs
@@ -1,10 +1,25 @@
-﻿namespace Assignment_4
+﻿using Assignment_4.interface_Ex2;
+
+namespace Assignment_4
 {
     internal class Program
     {
+        public static void print10NumberFromSeries(seriesByTwo series)
+        {
+            if(series == null)
+                return;
+            for(int i = 0;i<10;i++)
+            {
+                Console.WriteLine($" {series.current}");
+                series.GetNext();
+            }
+            series.Reset();
+            Console.WriteLine();
+        }
         static void Main(string[] args)
         {
-           
+           seriesByTwo series = new seriesByTwo();
+            print10NumberFromSeries(series);
         }
     }
 }
